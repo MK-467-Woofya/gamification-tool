1abe3b42daec9e781eaadb542d489d26a77680c3
Dante Filatow <80654569+s3901696@users.noreply.github.com>
2024-09-10 17:01:06 +1000

    Add files via upload

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { Button, Typography, Container, Box, CircularProgress } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
     const navigate = useNavigate();
 
     const goToProfile = () => {
-        navigate('/profile');
+        navigate('/profile');  // Navigation
     };
 
     return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            <p>This is the homepage of your React app.</p>
-            <button onClick={goToProfile}>Go to Profile Page</button>
-        </div>
+        <Container>
+            <Typography variant="h1" gutterBottom>
+                Welcome to Woofya
+            </Typography>
+            <Typography variant="body1" paragraph>
+                Enhance your dog's life with fun events and rewards!
+            </Typography>
+            <Button variant="contained" color="primary" onClick={goToProfile}>
+                Go to Profile Page
+            </Button>
+        </Container>
     );
 }
 
 export default HomePage;
-
--- frontend/src/components/ProfilePage.js
+++ frontend/src/components/ProfilePage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Button, Typography, Container, Box, CircularProgress } from '@mui/material';
 
 const ProfilePage = () => {
     const [user, setUser] = useState(null);
