50387ad30ac2785f9d13428013f7e959674f1b09
Paul Lubig <pg.lubig@gmail.com>
2024-10-01 00:22:57 +1000

    Bad merge. Re-implemented frontend routing and project structure. Fixed change in docker-compose.yml. Added to .gitignore for future implementation.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import { NavigationBar } from './components/ui/NavigationBar';
 import { HomePage } from './components/web/HomePage';
 import { LoginPage } from './components/web/LoginPage';
 import { LogoutPage } from './components/web/LogoutPage';
-import { LocationsPage } from './components/web/LocationsPage';
-import { EventsPage } from './components/web/EventsPage';
+import { LocationsPage } from './components/api/LocationsPage';
+import { EventsPage } from './components/api/EventsPage';
 import { ProfilePage } from './components/api/ProfilePage';
 import { CheckInsPage } from './components/api/CheckInsPage';
 import { QuestsPage } from './components/api/QuestsPage';
@@ -16,13 +16,23 @@ import { MarketplacePage } from './components/api/MarketplacePage';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/leaderboard" element={<LeaderboardPage />} />  {/* leaderboard */}
-            </Routes>
-        </Router>
+        <div className="App">
+            <Router>
+                <NavigationBar></NavigationBar>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/user/profile" element={<ProfilePage />} />
+                    <Route path="/locations" element={<LocationsPage />} />
+                    <Route path="/events" element={<EventsPage />} />
+                    <Route path="/user/checkins" element={<CheckInsPage />} />
+                    <Route path="/user/quests" element={<QuestsPage />} />
+                    <Route path="/user/leaderboard/" element={<LeaderboardPage />} />                    
+                    <Route path="/user/marketplace" element={<MarketplacePage />} />
+                    <Route path="/logout" element={<LogoutPage />} />
+                </Routes>
+            </Router>
+        </div>
     );
 }
 
