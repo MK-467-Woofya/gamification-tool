9d281726153250e47773f9c109a220ba0e652fbe
ZiyingLiu66 <104132043+ZiyingLiu66@users.noreply.github.com>
2024-10-09 20:26:42 +1100

    pictures and frontend structure

---
+++ frontend/src/components/api/MemoryGame.js
@@ -0,0 +1,17 @@
+// src/components/MemoryGame.js
+
+import React, { useState, useEffect } from 'react';
+import '../MemoryGame.css'; // FIXME：independent css file
+
+const MemoryGame = () => {
+    // game state and logic
+
+    return (
+        <div className="memory-game">
+            <h1>Doggy Memory Flippy</h1>
+            {/* game  interface here*/}
+        </div>
+    );
+};
+
+export default MemoryGame;
