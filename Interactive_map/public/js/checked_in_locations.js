f6be49d23b0269f1ffd459b05d6aab6f177a4ad7
Jerry <s3947309@student.rmit.edu.au>
2024-10-22 17:44:25 +1100

    chcekdin

--- Interactive_map/public/js/checked_in_locations.js
+++ Interactive_map/public/js/checked_in_locations.js
@@ -6,4 +6,4 @@ document.addEventListener("DOMContentLoaded", function() {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-}
+});
