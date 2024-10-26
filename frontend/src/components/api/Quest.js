19098d2b1ccec96df18be34c7b26b5273f72f483
maxowbricker <128575472+maxowbricker@users.noreply.github.com>
2024-10-26 14:14:50 +1100

    Basic reading data from the backend

--- frontend/src/components/api/Quest.js
+++ frontend/src/components/api/Quest.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Quest = ({ quest }) => {
+const Quest = ({ quest, progress }) => {
   return (
     <div style={{ margin: '20px', padding: '10px', border: '1px solid #ccc' }}>
       <h2>{quest.title}</h2>
       <p>Goal: {quest.goal} km</p>
-      <p>Progress: {quest.progress} km</p>
-      <progress value={quest.progress} max={quest.goal}></progress>
+      <p>Progress: {progress} km</p>
+      <progress value={progress} max={quest.goal}></progress>
     </div>
   );
 };
--- frontend/src/components/api/QuestsPage.js
+++ frontend/src/components/api/QuestsPage.js
@@ -1,29 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import { mockData, setLoggedInUser, getLoggedInUser } from '../../Data/QuestData'; // Adjust the path as necessary
 import Quest from './Quest';
-/**
- * Quests view where users are given tasks to complete in exchange for points
- * 
- */
+import axios from 'axios';
+
+// Load environment variables
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 export const QuestsPage = () => {
   const [quests, setQuests] = useState([]);
-
-  setLoggedInUser('user3');
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const loggedInUser = getLoggedInUser();
-    const userQuests = mockData.users.find(user => user.username === loggedInUser)?.quests;
-    if (userQuests) {
-      setQuests(userQuests);
-    }
+    // Get the current logged-in user ID from sessionStorage
+    const userId = sessionStorage.getItem('uid');
+    
+    // Fetch user quest progress from the backend
+    const fetchQuests = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}quests/user-progress/`, {
+          params: { user_id: userId },
+          headers: {
+            'Gamification-Api-Key': API_KEY
+          }
+        });
+        // Update the state with the fetched quest progress data
+        setQuests(response.data.results);
+      } catch (err) {
+        console.error('Error fetching quest data:', err);
+        setError('Could not fetch quest data. Please try again later.');
+      }
+    };
+
+    fetchQuests();
   }, []);
 
   return (
     <div>
       <h1>Quests</h1>
-      {quests.map(quest => (
-        <Quest key={quest.questId} quest={quest} />
-      ))}
+      {error && <p>{error}</p>}
+      {quests.length > 0 ? (
+        quests.map(questProgress => (
+          <Quest key={questProgress.id} quest={questProgress.quest} progress={questProgress.progress} />
+        ))
+      ) : (
+        <p>No quests available</p>
+      )}
     </div>
   );
 };
